f9485720461524b6d8faf08ce24eb509c18c4fd7
Bijay Jha <bijayjha12345@gmail.com>
2025-10-18 16:31:49 +0545

    Working on addTask() to display alert for empty input

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 
 const inputBox= document.getElementById("inputBox");
 const listOfTasks= document.getElementById("listOfTasks");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("The input field cannot be empty!! Please enter the task.");
+    }
+   
