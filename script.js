28bef03722be157060e0a14c76afaadb63833018
Bijay Jha <bijayjha12345@gmail.com>
2025-10-18 22:28:14 +0545

    Adding JS function to check and uncheck tasks using eventListener

--- script.js
+++ script.js
@@ -16,3 +16,12 @@ function addTask(){
     }
     inputBox.value = "";
 }
+
+listOfTasks.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
