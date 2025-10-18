b7d5342039f906868a32e7554c7d8eae766eba01
Bijay Jha <bijayjha12345@gmail.com>
2025-10-18 22:14:41 +0545

    If not empty, creating new list and adding it to listOfTasks, and creating span to have a cross to use as a remover

--- script.js
+++ script.js
@@ -6,4 +6,13 @@ function addTask(){
     if(inputBox.value === ''){
         alert("The input field cannot be empty!! Please enter the task.");
     }
-   
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listOfTasks.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+}
